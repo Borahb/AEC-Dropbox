c94ceba1096ab9fe5721975ac578b32694a2fe8a
Bhaskar Borah <borahb563@gmail.com>
2020-10-02 23:18:40 +0530

    Correct Login Page

--- lib/Screens/Login/student_screen.dart
+++ lib/Screens/Login/student_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_tpc/Screens/Login/Admin_components/body.dart';
+import 'package:project_tpc/Screens/Login/Stud_components/body.dart';
 
 class StudentLogin extends StatelessWidget {
 
